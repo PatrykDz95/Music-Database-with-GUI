6527501f454a8fd14241d804e58e720556fc723c
Patryk <patrykdziedzioch@gmail.com>
2018-03-19 22:08:06 +0100

    Added TableView to Controller class and adding properties

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import sample.model.Artist;
+import sample.model.Datasource;
+
 public class Controller {
+
+    @FXML
+    private TableView<Artist> artistTable;
+
+    public void listArtist(){
+        Task<ObservableList<Artist>> task = new GetAllArtistsTask();
+        artistTable.itemsProperty().bind(task.valueProperty());
+    }
+
+}
+
+class GetAllArtistsTask extends Task{
+    @Override
+    protected ObservableList<Artist> call() {
+        return FXCollections.observableArrayList(
+                Datasource.getInstance().queryArtist(Datasource.ORDER_BY_ASC));
+        );
+    }
 }
